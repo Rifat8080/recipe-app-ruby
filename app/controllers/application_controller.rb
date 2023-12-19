1c7874d8a745f1a3472c1551af570cfd043e3cfe
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2023-12-19 14:32:34 +0600

    Added inventories shows controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
-end
+    include Devise::Controllers::Helpers
+  
+    def after_sign_up_path_for(resource)
+      return unless resource.is_a?(User)
+  
+      recipes_path
+    end
+  end
+  
